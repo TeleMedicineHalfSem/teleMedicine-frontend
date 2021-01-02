74596b8f17e4868f69b97ef93dac264e7de797c2
Sawarni Swaroop <sawarni99@gmail.com>
2021-01-02 19:01:46 +0530

    Hello written

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       {
-        // App.js should be used only for routing and redirecting to different pages..
-        // Do not write any code here..
+        "Hello" // App.js should be used only for routing and redirecting to different pages..
+        // Do not write any code here..""
       }
     </div>
   );
