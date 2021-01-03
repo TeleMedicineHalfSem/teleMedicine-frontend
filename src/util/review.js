ad3ae769fca935d13818bfbdf0fdd92bd4532585
Sawarni Swaroop <sawarni99@gmail.com>
2021-01-03 19:00:02 +0530

    Refactored some codes.

--- src/util/review.js
+++
@@ -1,6 +0,0 @@
- const review = [
-    {id:1,Name:"Abhishek",Comment:"Very helpful.Allow easy and quick search with speed booking.Even maintains history of doctor visited."},
-    {id:2,Name:"Raj",Comment:"Very easy to maaintain history.Hassle free from older version of booking appointment via telephone. "}
-];
-
-export default review;
