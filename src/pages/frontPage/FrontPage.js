39d5296a0e1b34b16f1a0997ae760b64197dac51
Sawarni Swaroop <sawarni99@gmail.com>
2021-01-04 00:25:48 +0530

    Created Custom Button

---
+++ src/pages/frontPage/FrontPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./FrontPage.css";
+
+function FrontPage() {
+  return (
+    <div className="front-page">
+      <div className="front-page-front-view">
+        <div className="front-page-header"></div>
+        <div className="front-page-body"></div>
+      </div>
+      <div className="front-page-content"></div>
+      <div className="front-page-footer"></div>
+    </div>
+  );
+}
+
+export default FrontPage;
