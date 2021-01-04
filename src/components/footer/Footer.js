9d58f0d72411b9a3cf4a868f0246561a64dbbf77
Sawarni Swaroop <sawarni99@gmail.com>
2021-01-04 19:01:01 +0530

    Refactored some codes.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./footer.css";
+import "./Footer.css";
 
 function Footer() {
   return (
@@ -9,14 +9,14 @@ function Footer() {
           {/* Column1 */}
           <div className="col">
             <h4>EasyCare</h4>
-            <h1 className="list-unstyled">
+            <h2 className="list-unstyled">
               <li>A telemedicine solution</li>
               <li>Application for virtual doctor consultancy</li>
-            </h1>
+            </h2>
           </div>
           {/* Column2 */}
           <div className="col">
-            <h4>Why Us</h4>
+            <h5>Why Us</h5>
             <ui className="list-unstyled">
               <li>Cost efficient</li>
               <li>24*7 services available</li>
@@ -25,16 +25,22 @@ function Footer() {
           </div>
           {/* Column3 */}
           <div className="col">
-            <h4>Terms and Conditions</h4>
+            <h5>Terms and Conditions</h5>
             <ui className="list-unstyled">
-            <li>All information received by us from your registration on business-standard.com</li>
-            <li>or other digital products of Business Standard will be used</li>
-            <li>by Business Standard in accordance with our Privacy Policy. </li>
-            <li>Kindly read the below mentioned details.</li>
+              <li>
+                All information received by us from your registration on
+                business-standard.com
+              </li>
+              <li>
+                or other digital products of Business Standard will be used
+              </li>
+              <li>
+                by Business Standard in accordance with our Privacy Policy.{" "}
+              </li>
+              <li>Kindly read the below mentioned details.</li>
             </ui>
           </div>
         </div>
-        <hr />
         <div className="row">
           <p className="col-sm">
             &copy;{new Date().getFullYear()} EasyCare | All rights reserved |
