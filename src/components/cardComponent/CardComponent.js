f3b644c04dd2ca0bbb891f1fa9511313a428f01a
Sawarni Swaroop <sawarni99@gmail.com>
2021-01-04 19:48:26 +0530

    Resovled scrolling problem in Card

--- src/components/cardComponent/CardComponent.js
+++ src/components/cardComponent/CardComponent.js
@@ -1,21 +1,18 @@
-import React from 'react';
+import React from "react";
 //import {Card, Button, CardImg, CardTitle, CardText, CardDeck,CardSubtitle, CardBody} from 'reactstrap';
 import "./CardComponent.css";
 
-const CardStruct = ({img, alt, text}) => {
-    return(
-        <div className="row">
-  <div className="column">
-    <div className="card">
-    <img src={img} alt={alt}  className="responsive"/>
-      <p>{text}</p>
+const CardStruct = ({ img, alt, text }) => {
+  return (
+    <div className="card-row">
+      <div className="card-column">
+        <div className="card">
+          <img src={img} alt={alt} className="card-responsive" />
+          <p>{text}</p>
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
-    );
-
+  );
 };
 
-
-     
-export default CardStruct; 
+export default CardStruct;
