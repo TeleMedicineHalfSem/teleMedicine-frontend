0aaec012475068a2473c8b6fa76b015d52b533fd
Sawarni Swaroop <sawarni99@gmail.com>
2021-01-06 11:46:19 +0530

    created directories for signup and signin

---
+++ src/pages/signUpPage/SignUpPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SignupPage() {
+  return (
+    <div>
+      {
+        // TODO: Abhishek write here...
+        // To see this page goto http://localhost:3000/signup
+      }
+    </div>
+  );
+}
+
+export default SignupPage;
