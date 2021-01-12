5719f04dc87d60cb2f3f9921e39dfe36e1cad99c
Slokha <mailme.slokha@gmail.com>
2021-01-12 19:23:49 +0530

    nav code

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+let Nav =()=>{
+    return(
+        <nav>
+            <div className="name"><h1>EasyCare</h1></div>
+            <ul>
+                <li>Sign In</li>
+                <li>Sign Up</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
