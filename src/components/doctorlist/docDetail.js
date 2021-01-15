b553a67d4268c6a9dfae70d3dd1c6a7d87faca02
abhi2103ar7 <abhishekranjan@karunya.edu.in>
2021-01-15 13:03:37 +0530

    doctordetail dummy array added

--- src/components/doctorlist/docDetail.js
+++ src/components/doctorlist/docDetail.js
@@ -5,30 +5,31 @@ import  "../../App.css";
 import doctorDetails from "../../utils/detail";
 import Button from "../button/Button";
 import Divider from "../../components/divider/Divider"
-const details = doctorDetails.map((item) => {
-    return(
-        <div>           
-            <Row>
-                <Col xs="4">
-                    <img  src={item.Image} width="100px" height="100px"></img>
-                </Col>
-                <Col xs="4">
-                    <span className="detail-font-1">{item.Name}</span><br/>
-                    <span className="detail-font-2">{item.Specialization}</span>
-                    <p>Experience :  {item.Experience}</p>
-                </Col>
-                <Col xs="4">
-                    <Button color={"secondary"}>
-                        Chat
-                    </Button>
-                </Col>
-            </Row>
-            <Divider/>
-        </div>
-    );
-}); 
+ 
 
 function docDetail() {
+    const details = doctorDetails.map((item) => {
+        return(
+            <div>           
+                <Row>
+                    <Col xs="4">
+                        <img  src={item.Image} width="100px" height="100px"></img>
+                    </Col>
+                    <Col xs="4">
+                        <span className="detail-font-1">{item.Name}</span><br/>
+                        <span className="detail-font-2">{item.Specialization}</span>
+                        <p>Experience :  {item.Experience}</p>
+                    </Col>
+                    <Col xs="4">
+                        <Button color={"secondary"}>
+                            Chat
+                        </Button>
+                    </Col>
+                </Row>
+                <Divider/>
+            </div>
+        );
+    });
     return (
         <div>
             <Container>
