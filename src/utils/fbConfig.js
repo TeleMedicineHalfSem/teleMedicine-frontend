80a742547681d5b71c77c4c5e421d7fa78c0af0e
Sawarni Swaroop <sawarni99@gmail.com>
2021-01-21 22:24:25 +0530

    Added env file for firebase api key

--- src/utils/fbConfig.js
+++ src/utils/fbConfig.js
@@ -3,12 +3,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFBS40KjT4zBIc6bS2YD-gQOavWlz2aRk",
-  authDomain: "easycare-bd7a4.firebaseapp.com",
-  projectId: "easycare-bd7a4",
-  storageBucket: "easycare-bd7a4.appspot.com",
-  messagingSenderId: "485695445000",
-  appId: "1:485695445000:web:16ec065a6997973275d9bd"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
