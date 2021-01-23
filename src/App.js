f06705a013103bd75533b3653873093c25083730
Sawarni Swaroop <sawarni99@gmail.com>
2021-01-23 16:56:35 +0530

    added doctor page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import "./App.css";
 import FrontPage from "./pages/frontPage/FrontPage";
 import SignUpPage from "./pages/signUpPage/SignUpPage";
 import SignInPage from "./pages/signInPage/SignInPage";
-import PatientPage from "./pages/patientPage/PatientPage"
+import PatientPage from "./pages/patientPage/PatientPage";
+import DoctorPage from "./pages/doctorPage/DoctorPage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/doctor">
+          <DoctorPage />
+        </Route>
         <Route path="/patient">
           <PatientPage />
         </Route>
---
+++ src/pages/doctorPage/DoctorPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./DoctorPage.css";
+
+function DoctorPage() {
+  return (
+    <div>
+      {
+        // Test your codes here... Anugya & Slokha
+      }
+    </div>
+  );
+}
+
+export default DoctorPage;
