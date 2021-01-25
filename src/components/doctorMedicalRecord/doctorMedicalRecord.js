7fa38e6afa99b79030614e336653ba7d54851a2c
Anugya ram <anugyaram08@gmail.com>
2021-01-25 17:01:50 +0530

    created medical record

--- src/components/doctorMedicalRecord/doctorMedicalRecord.js
+++ src/components/doctorMedicalRecord/doctorMedicalRecord.js
@@ -1,21 +1,27 @@
 import React from "react";
+import "./DoctorMedicalRecord.css";
+import Divider from "../divider/Divider";
 
 const doctorRecord = ({patientName, patientProblem}) => {
     return(        
-        <div>
+        <div className="card">
             <div className="card-heading">
-                <h2>Recent Medical Cases</h2>
+                <h5>Recent Medical Cases</h5>
             </div>
-            <div className="card-body">
-                <div className="card-body-heading">
-                    <span>{patientName}</span>
-                </div>
-                <div className="card-body-description">
-                    <p>{patientProblem}</p>
-                </div>
-                <hr />
+            <div className="card-body-heading">
+                <span>{patientName}</span>
             </div>
-           
+            <div className="card-body-description">
+                <p>{patientProblem}</p>
+            </div>
+            <Divider
+                margin={{
+                top: "0px",
+                bottom: "10px",
+                left: "0px",
+                right: "5px",
+                }}
+            />
         </div>
     );
 };
