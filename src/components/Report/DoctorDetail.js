73388e116bd108a53c4233d837059d530b96fc08
Anugya ram <anugyaram08@gmail.com>
2021-03-07 16:47:50 +0530

    heading

---
+++ src/components/Report/DoctorDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./DoctorDetail.css";
+
+function DoctorDetail(){
+    return(
+        <div>
+            <p class="doctor-detail-heading">Doctor's Details</p>
+            
+        </div>
+
+    )
+}
+
+export default DoctorDetail;
--- src/pages/doctorReportPage/DoctorReportPage.js
+++ src/pages/doctorReportPage/DoctorReportPage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import DoctorDetail from "../../components/Report/DoctorDetail";
 
 function DoctorReportPage() {
   return (
     <div>
-      Hello
+      <DoctorDetail />
     </div>
   );
 }
