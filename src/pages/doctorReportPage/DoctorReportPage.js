776d2457bfe100c78a3e6ac57b117ed2cbf1582c
Anugya ram <anugyaram08@gmail.com>
2021-03-07 23:18:07 +0530

    doctor-report-with-margin

--- src/pages/doctorReportPage/DoctorReportPage.js
+++ src/pages/doctorReportPage/DoctorReportPage.js
@@ -1,12 +1,32 @@
 import React from "react";
+import "./DoctorReportPage.css";
 import DoctorDetail from "../../components/Report/DoctorDetail";
 import PatientDetail from "../../components/Report/PatientDetail";
+import Button from "../../components/button/Button";
+import NavBar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
 
 function DoctorReportPage() {
   return (
-    <div>
-      <DoctorDetail docName="Anugya" specilization="dentist" email="abc@mail.com"/>
-      <PatientDetail pName="Mayur" gender="male" dob="22-10-2000" age="21" />
+    <div className="doctor-report">
+      <div className="doctor-report-header">
+        <NavBar />
+      </div>
+      <div className="doctor-report-heading">
+        <p>Medical Information</p>
+      </div>
+      <div className="doctor-details">
+        <DoctorDetail docName="Anugya" specilization="dentist" email="abc@mail.com"/>
+      </div>
+      <div className="patient-details">
+        <PatientDetail pName="Mayur" gender="male" dob="22-10-2000" age="21" />
+      </div>
+      <div className="submit-btn">
+        <Button color={"secondary"} type={"submit"} size={"medium"}>Submit</Button>
+      </div>
+      <div className="doctor-report-footer">
+        <Footer />
+      </div>
     </div>
   );
 }
