cef61c4d3e3ed75430bf24e5241c9c8d4acec3ab
Anugya ram <anugyaram08@gmail.com>
2021-03-08 15:23:08 +0530

    created banner

--- src/pages/medicalRecordPage/MedicalRecordPage.js
+++ src/pages/medicalRecordPage/MedicalRecordPage.js
@@ -10,9 +10,15 @@ function MedicalRecord(){
                 <Navbar />
             </div>
             <div className="medical-record-body">
-                <div className="medical-record-heading">
-                    <p>Easy Care</p>
+                <div className="medical-record-banner">
+                    <div className="medical-record-heading">
+                        <p className="medical-record-heading-left">Easy Care</p>
+                        <p className="medical-record-heading-right">Medical Record</p>
+                    </div>
+                    <p className="medical-record-sub-heading">Online Consultancy Service</p>
+                    <p className="medical-record-date">14 November 2020</p>
                 </div>
+                <hr className="blue-line"></hr>
             </div>
             <div className="medical-record-footer">
                 <Footer />
