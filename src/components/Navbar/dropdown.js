84cf9a6dba40d6f40efac1adeb5fb4d56b8a4306
Sawarni Swaroop <sawarni99@gmail.com>
2021-03-30 21:57:51 +0530

    Added buttons and implemneted it in navbar dropdowns

--- src/components/Navbar/dropdown.js
+++ src/components/Navbar/dropdown.js
@@ -2,15 +2,46 @@ import React from "react";
 import "./Navbar.css";
 import { connect } from "react-redux";
 import { signOut } from "../../actions/authActions";
+import { useHistory } from "react-router-dom";
 
-function Dropdown({ username, email, initials, isVisible, toggle, signOut }) {
+function Dropdown({ username, email, isVisible, toggle, signOut, profile }) {
+  const history = useHistory();
+  let buttonView = null;
   const onClickLogOut = () => {
     signOut();
   };
 
   const onClickProfile = () => {
-    // Redirect to profile page...
+    history.push("/doctor");
   };
+
+  const onClickMedicalRecords = () => {
+    history.push("/patientMR");
+  };
+
+  const onClickDoctors = () => {
+    history.push("/patient");
+  };
+
+  if (!profile.isEmpty && profile.isDoctor) {
+    buttonView = (
+      <div onClick={onClickProfile} className="dropdown-button">
+        Profile
+      </div>
+    );
+  } else {
+    buttonView = (
+      <div>
+        <div onClick={onClickMedicalRecords} className="dropdown-button">
+          Medical Records
+        </div>
+        <div onClick={onClickDoctors} className="dropdown-button">
+          Doctors
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
       {isVisible ? (
@@ -23,9 +54,7 @@ function Dropdown({ username, email, initials, isVisible, toggle, signOut }) {
               <div className="dropdown-email">{email}</div>
             </div>
           </div>
-          <div onClick={onClickProfile} className="dropdown-button">
-            Profile
-          </div>
+          {buttonView}
           <div onClick={onClickLogOut} className="dropdown-button">
             Log Out
           </div>
@@ -34,4 +63,10 @@ function Dropdown({ username, email, initials, isVisible, toggle, signOut }) {
     </>
   );
 }
-export default connect(null, { signOut })(Dropdown);
+const mapStateToProps = (state) => {
+  return {
+    profile: state.firebase.profile,
+  };
+};
+
+export default connect(mapStateToProps, { signOut })(Dropdown);
